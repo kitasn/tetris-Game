0e76347f90efc9a97d2c9e68168a95c4a7c2bacb
kitasn <65902963+kitasn@users.noreply.github.com>
2020-12-30 21:30:51 +0700

    Add files via upload

---
+++ const.js
@@ -0,0 +1,49 @@
+const _WIDTH = 200;
+const _HEIGHT = 400;
+const _COLS = 10;
+const _ROWS = 20;
+const _SIZE = 20;
+
+const _NEXTWIDTH = 90;
+const _NEXTHEIGHT =75;
+const _NEXTCOLS = 6;
+const _NEXTROWS = 5;
+const _NEXTSIZE = 15;// 
+
+const _ = null;
+const x = "x";
+const _colorBr = 'rgb(180, 180, 180,0.8)';
+const _colorBl = 'red';
+
+const _BaseBrick = [
+					[
+						[x,x,x,x]
+					],
+					[
+						[x,x],
+						[x,x]
+					],
+					[
+						[x,x,x],
+						[_,_,x]
+					],
+					[
+						[_,_,x],
+						[x,x,x]
+					],
+					[
+						[x,_],
+						[x,x],
+						[_,x]
+					],
+					[
+						[_,x],
+						[x,x],
+						[x,_]
+					],
+					[
+						[x,x,x],
+						[_,x,_]
+					]
+				];
+
